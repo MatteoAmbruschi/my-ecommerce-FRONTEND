1b28e1bf74a82510b2b04f6a6bd79d1bd776d287
Matteo <matteoambruschi10@gmail.com>
2024-06-15 19:30:44 +0200

    prova token

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -20,7 +20,9 @@ export default function SignIn({charge, setCharge}) {
         const token = localStorage.getItem('authToken');
         if(token) {
           const response = await axios.get(`${process.env.NEXT_PUBLIC_URL}login`, {
-            headers: { Authorization: `Bearer ${token}` },
+            headers: {           
+              'Authorization': token,
+              'Content-Type': 'application/json' },
             withCredentials: true
           });
           if (response.status === 200) {
