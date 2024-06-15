9e3e8634fcf2baf327eb5fd3c5997ebecb92af0b
Matteo <matteoambruschi10@gmail.com>
2024-06-15 19:42:48 +0200

    no Content-Type

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -18,9 +18,7 @@ export default function Profile({ charge, setCharge}) {
       try {
         const token = localStorage.getItem('authToken');
         const response = await axios.get(process.env.NEXT_PUBLIC_URL + 'dashboard', {
-          headers: {           
-            'Authorization': token,
-            'Content-Type': 'application/json' },
+          headers: { Authorization: token },
           withCredentials: true
         });
         if (response.status === 200) {
