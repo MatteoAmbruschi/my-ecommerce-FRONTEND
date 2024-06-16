9141f6d473eb2a74af814496bf41fab95272a447
Matteo <matteoambruschi10@gmail.com>
2024-06-16 15:28:41 +0200

    in Layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,6 +1,41 @@
 import { motion } from 'framer-motion'
 import styles from './layout.module.css'
 import { useEffect } from 'react'
+import Router from 'next/router'
+
+
+export const fixTimeoutTransition = (timeout) => {
+    Router.events.on('beforeHistoryChange', () => {
+        // Create a clone of every <style> and <link> that currently affects the page.
+        const nodes = document.querySelectorAll('link[rel=stylesheet], style:not([media=x])');
+        const copies = [...nodes].map((el) => el.cloneNode(true));
+
+        for (let copy of copies) {
+            // Remove Next.js' data attributes so the copies are not removed from the DOM in the route change process.
+            copy.removeAttribute('data-n-p');
+            copy.removeAttribute('data-n-href');
+
+            // Add duplicated nodes to the DOM.
+            document.head.appendChild(copy);
+        }
+
+        const handler = () => {
+            // Emulate a `.once` method using `.on` and `.off`
+            Router.events.off('routeChangeComplete', handler);
+
+            window.setTimeout(() => {
+                for (let copy of copies) {
+                    // Remove previous page's styles after the transition has finalized.
+                    if (document.head.contains(copy)) {
+                        document.head.removeChild(copy);
+                    }
+                }
+            }, timeout);
+        };
+
+        Router.events.on('routeChangeComplete', handler);
+    });
+};
 
 
 
@@ -81,6 +116,8 @@ export default function Layout({children}) {
         loadGrained();
 
         window.scrollTo(0, 0);
+
+        fixTimeoutTransition(1000);
     }, [])
 
     return (
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,51 +2,12 @@ import "@/styles/globals.css";
 import { AnimatePresence } from "framer-motion";
 import Nav from "@/components/nav/Nav";
 import Footer from "@/components/footer/Footer";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Cart from "@/components/cart/Cart";
-import Router from 'next/router'
-
-export const fixTimeoutTransition = (timeout) => {
-    Router.events.on('beforeHistoryChange', () => {
-        // Create a clone of every <style> and <link> that currently affects the page.
-        const nodes = document.querySelectorAll('link[rel=stylesheet], style:not([media=x])');
-        const copies = [...nodes].map((el) => el.cloneNode(true));
-
-        for (let copy of copies) {
-            // Remove Next.js' data attributes so the copies are not removed from the DOM in the route change process.
-            copy.removeAttribute('data-n-p');
-            copy.removeAttribute('data-n-href');
-
-            // Add duplicated nodes to the DOM.
-            document.head.appendChild(copy);
-        }
-
-        const handler = () => {
-            // Emulate a `.once` method using `.on` and `.off`
-            Router.events.off('routeChangeComplete', handler);
-
-            window.setTimeout(() => {
-                for (let copy of copies) {
-                    // Remove previous page's styles after the transition has finalized.
-                    if (document.head.contains(copy)) {
-                        document.head.removeChild(copy);
-                    }
-                }
-            }, timeout);
-        };
-
-        Router.events.on('routeChangeComplete', handler);
-    });
-};
-
 
 export default function App({ Component, pageProps, router }) {
   const [charge, setCharge] = useState(0);
   const [openCart, setOpenCart] = useState(0)
-
-  useEffect(() => {
-    fixTimeoutTransition(1000);
-  }, [])
 
   return (
     <main>
