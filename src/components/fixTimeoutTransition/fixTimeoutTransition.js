934371e96db6b97f72e884d25aca549006747b5a
Matteo <matteoambruschi10@gmail.com>
2024-06-16 15:32:28 +0200

    in componente app

---
+++ src/components/fixTimeoutTransition/fixTimeoutTransition.js
@@ -0,0 +1,29 @@
+import Router from 'next/router'
+
+export const fixTimeoutTransition = (timeout) => {
+    Router.events.on('beforeHistoryChange', () => {
+        const nodes = document.querySelectorAll('link[rel=stylesheet], style:not([media=x])');
+        const copies = [...nodes].map((el) => el.cloneNode(true));
+
+        for (let copy of copies) {
+            copy.removeAttribute('data-n-p');
+            copy.removeAttribute('data-n-href');
+
+            document.head.appendChild(copy);
+        }
+
+        const handler = () => {
+            Router.events.off('routeChangeComplete', handler);
+
+            window.setTimeout(() => {
+                for (let copy of copies) {
+                    if (document.head.contains(copy)) {
+                        document.head.removeChild(copy);
+                    }
+                }
+            }, timeout);
+        };
+
+        Router.events.on('routeChangeComplete', handler);
+    });
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,18 @@ import "@/styles/globals.css";
 import { AnimatePresence } from "framer-motion";
 import Nav from "@/components/nav/Nav";
 import Footer from "@/components/footer/Footer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Cart from "@/components/cart/Cart";
+import { fixTimeoutTransition } from "@/components/fixTimeoutTransition/fixTimeoutTransition";
 
 export default function App({ Component, pageProps, router }) {
   const [charge, setCharge] = useState(0);
   const [openCart, setOpenCart] = useState(0)
+
+  useEffect(() => {
+    fixTimeoutTransition(900);
+  }, [])
+
 
   return (
     <main>
