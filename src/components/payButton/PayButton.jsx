c7941d011e29f3c676c9fbaa8b3f5e5b98bcf272
Matteo <matteoambruschi10@gmail.com>
2024-06-22 18:51:38 +0200

    senza userId

--- src/components/payButton/PayButton.jsx
+++ src/components/payButton/PayButton.jsx
@@ -1,39 +1,13 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
 const PayButton = ({ cartItems }) => {
     const router = useRouter();
-    const [user, setUser] = useState(null);
-  
-    useEffect(() => {
-        const fetchUserData = async () => {
-          try {
-            const token = localStorage.getItem('authToken');
-            const response = await axios.get(process.env.NEXT_PUBLIC_URL + 'dashboard', {
-              headers: { Authorization: token },
-              withCredentials: true
-            });
-            if (response.status === 200) {;
-              setUser(response.data);
-            } else {
-              router.push('/sign-in', undefined, { scroll: false })
-            }
-          } catch (error) {
-            router.push('/sign-in', undefined, { scroll: false })
-            console.error('Error fetching user data:', error);
-          }
-        };
-    
-        fetchUserData();
-      }, []);
-
 
     const handleCheckout = async () => {
-        if(user && cartItems && cartItems[0].total_elements > 0){
+        if(cartItems && cartItems[0].total_elements > 0){
             axios.post(`${process.env.NEXT_PUBLIC_URL}stripe/create-checkout-session`, {
                 cartItems,
-                userId: user.email
             }).then((res) => {
                 if(res.data.url){
                     router.push(res.data.url, undefined, { scroll: false })
